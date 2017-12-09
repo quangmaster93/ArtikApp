6956eed05f00ef07b142dc13d2e64f1eb7e4ca11
DINH <dinhph@dekiconnect.com>
2017-12-09 16:22:53 +0700

    + implement Network

--- app/components/DeviceComponent/DeviceComponent.js
+++ app/components/DeviceComponent/DeviceComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Network from '../../api/Network'
 import {
     View,
     Text,
@@ -15,42 +16,25 @@ export default class DeviceComponent extends Component {
     componentDidMount() {
         this.getUserInfo(this.props.token);
     }
-    getUserInfo(token) {
+
+    getUserInfo() {
         let that = this;
-        let url = `https://api.artik.cloud/v1.1/users/self`
-        fetch(url, {
-            method: "get",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
+        Network.get('/users/self', (responseJson) => {
+            console.log('/users/self')
+            console.log(responseJson)
+            let userId = JSON.parse(responseJson._bodyInit).data.id;
+            that.getDevices(userId)
         })
-            .then(responseJson => {
-                let userId = JSON.parse(responseJson._bodyInit).data.id;
-                that.getDevices(userId, token)
-            })
-            .catch(error => {
-                console.error(error);
-            });
     }
-    getDevices(userId, token) {
+
+    getDevices(userId) {
         let that = this;
-        let url = `https://api.artik.cloud/v1.1/users/${userId}/devices?count=100&includeProperties=false&includeShareInfo=false`;
-        fetch(url, {
-            method: "get",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
+        Network.get(`/users/${userId}/devices?count=100&includeProperties=false&includeShareInfo=false`, (responseJson) => {
+            let devices = JSON.parse(responseJson._bodyInit).data.devices;
+            that.setState({ devices: devices });
         })
-            .then(responseJson => {
-                let devices = JSON.parse(responseJson._bodyInit).data.devices;
-                that.setState({ devices: devices });
-            })
-            .catch(error => {
-                console.error(error);
-            });
     }
+
     render() {
         return (
             <View>
---
+++ app/config/settings.js
@@ -0,0 +1,9 @@
+const AppConfig = {
+    grant_type: "authorization_code",
+    redirect_uri: "cloud.artik.example.hellocloud://oauth2callback",
+    state: "abcdefgh",
+    code_verifier: "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk",
+    client_id: "0c2cefcfe2f245f58e053c31fa2241cb",
+    endpoint: 'https://api.artik.cloud/v1.1'
+}
+export { AppConfig }
